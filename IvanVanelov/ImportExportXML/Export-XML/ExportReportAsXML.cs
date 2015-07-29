993ab29d8e1c841e49ef205af60132cde7cec0e1
Extensa <ragnar245@gmail.com>
2015-07-29 19:11:46 +0300

    xml update

--- IvanVanelov/ImportExportXML/Export-XML/ExportReportAsXML.cs
+++ IvanVanelov/ImportExportXML/Export-XML/ExportReportAsXML.cs
@@ -1,9 +1,8 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.Linq;
 using System.Text;
-using System.Threading.Tasks;
-using System.Xml.Linq;
+using System.Xml;
+using System.Data;
 using DB_Mapping;
 
 
@@ -13,32 +12,74 @@ namespace Export_XML
     {
         static void Main()
         {
-            var context = new SupermarketChainEntities();
-            var vendors = context.Vendors
-                .OrderBy(v => v.VendorName)
-                .Select(v => new
-                {
-                    v.VendorName,
-                    Products = v.Products
-                        .OrderBy(p => p.ProductName)
-                        .Select(p => p.ProductName)
-                });
+            ExportSales("20-Jul-2014", "22-Jul-2014");
+        }
 
-            var xmlreport = new XElement("report");
-            foreach (var vendor in vendors)
+        public static void ExportSales(string startDate, string endDate)
+        {
+            var start = DateTime.Parse(startDate);
+            var end = DateTime.Parse(endDate);
+            ExportSales(start, end);
+        }
+
+        public static void ExportSales(DateTime startDate, DateTime endDate)
+        {
+            var encoding = Encoding.GetEncoding("utf-8");
+            string documentPath = Environment.GetFolderPath(Environment.SpecialFolder.Desktop) + "\\report.xml";
+            using (var writer = new XmlTextWriter(documentPath,encoding))
             {
-                var xmlvendor = new XElement("vendor");
-                xmlvendor.Add(new XAttribute("name", vendor.VendorName.Trim()));
-                xmlreport.Add(xmlvendor);
-                foreach (var product in vendor.Products)
+                writer.Formatting = Formatting.Indented;
+                writer.IndentChar = '\t';
+                writer.Indentation = 1;
+
+                writer.WriteStartDocument();
+                writer.WriteStartElement("sales");
+                WriteSalesToWriter(writer, startDate, endDate);
+                writer.WriteEndDocument();
+            }
+        }
+
+        private static void WriteSalesToWriter(XmlWriter writer, DateTime startDate, DateTime endDate)
+        {
+            using (var db = new SupermarketChainEntities())
+            {
+                foreach (var vendor in db.Vendors.ToList())
                 {
-                    xmlvendor.Add(new XElement("product", product.Trim()));
+                    var totalSalesSumByDate = db.Sales
+                        .Where(sale =>
+                            sale.Product.VendorId == vendor.Id &&
+                            sale.DateofSale >= startDate &&
+                            sale.DateofSale <= endDate)
+                        .OrderBy(sale => sale.DateofSale)
+                        .GroupBy(sale => sale.DateofSale)
+                        .Select(group => new
+                        {
+                            Date = group.Key,
+                            TotalSum = group.Sum(g => g.PricePerUnit * g.Quantity)
+                        })
+                        .ToList();
+
+                    if (totalSalesSumByDate.Any())
+                    {
+                        writer.WriteStartElement("sale");
+                        writer.WriteAttributeString("vendor", vendor.VendorName);
+                        foreach (var sale in totalSalesSumByDate)
+                        {
+                            AddSaleToVendor(writer, sale.Date, sale.TotalSum);
+                        }
+
+                        writer.WriteEndElement();
+                    }
                 }
             }
+        }
 
-            string path = Environment.GetFolderPath(Environment.SpecialFolder.Desktop);
-            var xmlDocument = new XDocument(xmlreport);
-            xmlDocument.Save(path + "\\report.xml");
+        private static void AddSaleToVendor(XmlWriter writer, DateTime date, decimal totalSum)
+        {
+            writer.WriteStartElement("summary");
+            writer.WriteAttributeString("date", date.ToString("d-MMM-yyyy"));
+            writer.WriteAttributeString("total-sum", totalSum.ToString("F2"));
+            writer.WriteEndElement();
         }
     }
 }
