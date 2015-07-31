acf6afb50fbc9940c73be9c7cc6b3b6cb2cbfd91
tarlit <tarlitan@gmail.com>
2015-07-31 09:56:11 +0300

    Create JSON files

---
+++ CreateJSON/Markets.ConsoleClient/Program.cs
@@ -0,0 +1,27 @@
+﻿namespace Markets.ConsoleClient
+{
+    using System;
+    using Markets.Data;
+    using Markets.Data.Migrations;
+    using Markets.DataManipulation;
+    using System.Data.Entity;
+
+    public class Program
+    {
+        public static void Main()
+        {
+
+            var db = new ChainOfSupermarketsContext();
+
+            // JSON Reports
+            string reportsDirectoryPath = @"..\..\Json-Reports";
+            var json = new JsonReport(db, reportsDirectoryPath);
+            DateTime start = new DateTime(27/7/2015);
+            DateTime end = new DateTime(28/7/2015);
+            json.ProductSalesForPeriod(start, end);
+            Console.WriteLine("JSON reports generate.");
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ CreateJSON/Markets.Model/ProductTotalSale.cs
@@ -0,0 +1,22 @@
+﻿using Newtonsoft.Json;
+
+namespace Markets.Model
+{
+    public class ProductTotalSale
+    {
+        [JsonProperty("product-id")]
+        public int ProductId { get; set; }
+
+        [JsonProperty("product-name")]
+        public string ProductName { get; set; }
+
+        [JsonProperty("vendor-name")]
+        public string VendorName { get; set; }
+
+        [JsonProperty("total-quantity-sold")]
+        public decimal QuantitySold { get; set; }
+
+        [JsonProperty("total-incomes")]
+        public decimal TotalIncomes { get; set; }
+    }
+}
